d45245c860ba3dcb674a915f17028d05184949b0
Zach Myre <myrezach@live.com>
2023-05-21 20:08:40 -0500

    more ts config stuff

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -29,6 +29,10 @@ const config = {
       },
     ],
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/no-unsafe-assignment": "off",
+    "@typescript-eslint/no-unsafe-member-access": "off",
+    "@typescript-eslint/restrict-template-expressions": "off",
+    "@typescript-eslint/no-explicit-any": "off",
   },
 };
 
